ef688ac0c7f8017ef7c512f4304e898c37abe2a4
Ruslan Shapoval <shapoval.websites@gmail.com>
2024-01-09 14:08:18 +0200

    basic app

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function Header() {
+  return (
+    <header>
+      <Link href="/">Home</Link>
+      <Link href="/blog">Blog</Link>
+      <Link href="/about">About</Link>
+    </header>
+  )
+}
